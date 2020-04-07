79e17e82a2c326001ce25cfc6f2ffe79f849611f
michaelnunn3 <michaelnunn@ufl.edu>
2020-04-07 11:52:17 -0500

    Counting Elements

---
+++ Problems/ccd7.py
@@ -0,0 +1,12 @@
+# LeetCode Problem #: Counting Elements
+# Michael Nunn
+
+# If running on LeetCode you must change class name to "Solution"
+
+class elements:
+    def countElements(self, arr: List[int]) -> int:
+        count: int = 0
+        for i in range(len(arr)):
+            if (arr[i]+1 in arr):
+                count += 1
+        return count

f603be7dec2c9f02ee4722b726e2a2e463bc6cc6
michaelnunn3 <michaelnunn@ufl.edu>
2020-04-10 15:18:55 -0500

    Min Stack

---
+++ Problems/0155.java
@@ -0,0 +1,34 @@
+// LeetCode Problem 155: Min Stack
+
+// If running on LeetCode you must change class name to "Solution"
+
+import java.util.*;
+
+class MinStack {
+        
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    
+    public void push(int x) {
+        if(x <= min){          
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if(stack.pop() == min) {
+            min=stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+
+}

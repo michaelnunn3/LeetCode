88475216eff0c10203ff5f7a36d3d0a929ba5118
michaelnunn3 <michaelnunn@ufl.edu>
2020-04-22 11:07:12 -0500

    Subarray Sum Equals K

--- Problems/0560.java
+++ Problems/0560.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 class sumK {
     public int subarraySum(int[] nums, int k) {
         
-        HashMap<Integer, Integer> total = new HashMap();
+        HashMap<Integer, Integer> total = new HashMap<Integer, Integer>();
         total.put(0,1);
         int sum = 0;
         int result = 0;

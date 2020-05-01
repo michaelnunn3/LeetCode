5368dc1bfa91a14f67a610fc779f88783669f8c1
michaelnunn3 <michaelnunn@ufl.edu>
2020-05-01 10:17:24 -0500

    First Bad Version

--- Problems/0278.java
+++ Problems/0278.java
@@ -4,23 +4,31 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-      public class Solution extends VersionControl {
-        public int firstBadVersion(int n) {
-            // Implement binary search
-            int left = 1;
-            int right = n;
-            int ans = -1;
-            
-            while (left<=right) {
-                int mid = left + (right - left) / 2;
-                if(isBadVersion(mid)) {
-                    ans = mid;
-                    right = mid - 1;
-                }
-                else {
-                    left = mid + 1;
-                }
-            }
-            return ans;
-        }
+class BadVersion {
+
+    public boolean isBadVersion(int n) {
+        // Method is a placeholder to avoid errors
+        // If running on leetcode remove and change
+        // class name to Solution
+        return true;
     }
+
+    public int firstBadVersion(int n) {
+        // Implement binary search
+        int left = 1;
+        int right = n;
+        int ans = -1;
+        
+        while (left<=right) {
+            int mid = left + (right - left) / 2;
+            if(isBadVersion(mid)) {
+                ans = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
